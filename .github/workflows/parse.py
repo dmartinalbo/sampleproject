10c0f3cb9d11b9ee5d1351f1f6e1717cb24c8553
Daniel Martín-Albo <dmas@wiris.com>
2020-11-06 16:22:03 +0100

    [kb-6529] testing ci/cd

--- .github/workflows/parse.py
+++ .github/workflows/parse.py
@@ -6,17 +6,20 @@ import json
 
 p = re.compile("\[[kK][bB]-(\d+)\]")
 
-labels = json.loads(sys.argv[1])
-print(labels)
-exit(0)
+commits = json.loads(sys.argv[1])
+
 url = "https://wiris.kanbanize.com/index.php/api/kanbanize/add_comment/"
 
-for label in labels:
-    o = p.finditer(label)
-    for match in o: 
-        filtered_commit_message = p.sub("",label).strip()
+for commit in commits:
+    commit_message = commit["message"]
+    commit_username = commit["author"]["username"]
+    commit_url = commit["url"]
+
+    for match in p.finditer(commit_message): 
+        filtered_commit_message = p.sub("", commit_message).strip()
         taskid = match.groups()[0]
        
         # todo: add link to commit or repo and include who made the commit
-        requests.post(url, json={"taskid": taskid, "comment": filtered_commit_message}, headers={"apikey": os.environ["KEY_API_KANBANIZE"]})
+        comment = f"{commit_username}: {filtered_commit_message} \n {commit_url}"
+        requests.post(url, json={"taskid": taskid, "comment": comment}, headers={"apikey": os.environ["KEY_API_KANBANIZE"]})
 
